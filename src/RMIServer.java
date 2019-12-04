92fea9639effde07660eebecd741c8f0c788471f
nevinhall <46722804+nevinhall@users.noreply.github.com>
2019-12-04 19:34:31 +0000

    configured rmi server

--- src/RMIServer.java
+++ src/RMIServer.java
@@ -1,4 +1,21 @@
+import java.net.MalformedURLException;
+import java.rmi.AlreadyBoundException;
+import java.rmi.Naming;
+import java.rmi.RemoteException;
+import java.rmi.registry.LocateRegistry;
+import java.util.ArrayList;
 
 public class RMIServer {
 
-}
+	public static void main(String args[]) throws RemoteException, MalformedURLException, AlreadyBoundException{  
+		ArrayList<Object> villainsToBeSent = new  ArrayList<Object>();
+		
+		Admin serverAdmin = new Admin(villainsToBeSent);
+
+		LocateRegistry.createRegistry(8000);
+		
+		Naming.bind("server", serverAdmin);
+	}
+	
+
+	}
